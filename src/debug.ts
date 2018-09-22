a2321a8ae711d4b808bbe4a3942e78398eb8ac5b
VitahaKryvenko <gerzoh1@gmail.com>
2018-09-22 16:53:31 +0300

    added debug module

---
+++ src/debug.ts
@@ -0,0 +1,17 @@
+export let enabled : boolean = true;
+
+
+export function assertNeverType(suspect: never) {
+    if (!enabled) return;
+
+    throw new Error(
+        `Ahtung! Assertion for ${suspect} to be of type 'never' has fired.`
+    );
+}
+export function unreachable(){
+    if (!enabled) return;
+
+    throw new Error(
+        'Ahtung! Program workflow has reached an unreachable line of code.'
+    );
+}
--- veetaha.ts
+++ veetaha.ts
@@ -1,20 +1,3 @@
-export interface ObjectAsMap<T = unknown> {
-    [key: string] : T;
-}
-
-export function isObjectAsMap<T = unknown>(suspect: unknown) : suspect is ObjectAsMap<T> {
-    return Boolean(
-        suspect && (typeof suspect === 'object' || typeof suspect === 'function')
-    );
-}
-
-export function deepCopy<T>(obj: T) : T {
-    if (typeof obj === 'object' && obj){
-        const copy: T = Object.create(Object.getPrototypeOf(obj));
-        for (const key of Object.getOwnPropertyNames(obj)) {
-            (copy as ObjectAsMap)[key] = deepCopy((obj as ObjectAsMap)[key]);
-        }
-        return copy;
-    }
-    return obj;
-}
+import * as Debug from './src/debug';
+export { Debug };
+export * from './src/general';
