28f568037e3466f340b10f41e990460f7ef91f35
VitahaKryvenko <gerzoh1@gmail.com>
2018-09-29 19:12:26 +0300

    added Math module to exports

--- veetaha.ts
+++ veetaha.ts
@@ -4,6 +4,7 @@ import * as Strings from './src/strings';
 import * as Fs      from './src/filesystem';
 import * as Types   from './src/types';
 import * as JsonFs  from './src/json-file-storage';
+import * as Math    from './src/math';
 
 
-export { Objects, Debug, Strings, Fs, Types, JsonFs };
+export { Objects, Debug, Strings, Fs, Types, JsonFs, Math };
